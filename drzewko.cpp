ba3cad92cfd67521ce2614e0e0a1bdc979ec73ec
NobodyImpo <norbertbak90@gmail.com>
2025-03-06 21:55:53 +0100

    dodałem funkcje wyodrebnij

--- drzewko.cpp
+++ drzewko.cpp
@@ -2,7 +2,34 @@
 #include <fstream>
 using namespace std;
 
+string wyodrebnij(string linia, int cel)
+{
+    string odpowiedz;
+    int przecinek=1;
+    for(int i=0;i<sizeof(linia);i++)
+    {
+        if(przecinek==cel){
+            odpowiedz=odpowiedz+linia[i];
+        }
+        if(linia[i]==',')
+        {
+            przecinek++;
+        }
+        if(przecinek>cel)
+        {
+            break;
+        }
+
+    }
+    return odpowiedz;
+}
+
 int main(){
 
+    ifstream odczyt("student_performance_dataset.csv");
+    string linia;
+    while(getline(odczyt, linia)){
+        cout << wyodrebnij(linia, 10) << endl;
+    }
     return 0;
 }
