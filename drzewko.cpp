6257a52cdebed0fd827da1efb637b22c6a347837
NobodyImpo <163216849+NobodyImpo@users.noreply.github.com>
2025-03-07 22:25:45 +0100

    naprawione odczytywanie bazy danych

--- drzewko.cpp
+++ drzewko.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <vector>
 using namespace std;
 
-string wyodrebnij(string linia, int cel)
+string wyodrebnij(string linia, int cel)//funkcja wyodrebnia konkretną komór z wiersza
 {
     string odpowiedz="";
-    int przecinek=1;
+    int przecinek=0;
     for(int i=0;i<linia.size();i++)
     {
         if(linia[i]==','){przecinek++;}
@@ -16,14 +16,14 @@ string wyodrebnij(string linia, int cel)
     return odpowiedz;
 }
 
-float okresl_entropie(int a[], int size)
+float okresl_entropie(vector<vector<int>> a)
 {
     int max=0;
     int min=0;
     float suma=0;
     for(int i=0; i<size; i++)
     {
-        suma=suma+a[i];
+        suma=suma+a[i][9];
     }
     max=a[0];
     for(int i=1; i<size; i++)
@@ -38,57 +38,75 @@ float okresl_entropie(int a[], int size)
     return (max-min)/suma;   
 }
 
-int liczba_wpisuw=0;
-int liczba_atrybutuw=1;
+bool czy_duplikat(vector<string> a, string cel)//sprawdza czy wartosc jest duplikatem
+{
+    for(int i=0; i < a.size();i++)
+    {
+        if(a[i]==cel)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int znajdz_duplikat(vector<string> a, string cel)//znajduje pozycje duplikatu
+{
+    for(int i=0; i < a.size();i++)
+    {
+        if(a[i]==cel)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main(){
 
     ifstream odczyt("student_performance_dataset.csv");
-    string linia;
+    string wiersz;
+
+    int liczba_atrybutow=1;//określa liczbe atrybutów w bazie danych
+
+    vector<vector<int>> dane;//vector 2d do przechowywania danych z bazy
+    vector<string> slownik;//przechowuje stringi z bady danych
+    string bin;//przechowuje informacje podreczne
     
-    while(getline(odczyt, linia)){
-        cout << linia <<endl;
-    }
-    cout << endl << endl << endl;
-    while(getline(odczyt, linia)){
-        cout << linia <<endl;
-    }
-    liczba_atrybutuw=9;
-    liczba_wpisuw--;
-    //normalizacja
-    int dane[liczba_atrybutuw][liczba_wpisuw][1];
-    string pomocnicza_tablica[liczba_wpisuw];
-    int b=0;
-    int duplikat=0;
-    for(int i=0; i<liczba_atrybutuw; i++)
-    {
-        for(int a=0; a<liczba_wpisuw; a++)
+    int liczba_iteracji=0;//określa liczbe iteracji poniższego while
+    while(getline(odczyt, wiersz)){//odczytanie danych i transport do wektora dane i uzupełnienie słownika
+        if(liczba_iteracji==0)//przewsza iteracja to nagłówki więc je wykluczam z danych
         {
-            pomocnicza_tablica[a]="";
-        }
-        b=0;
-        while(getline(odczyt, linia)){
-            duplikat=0;
-            
-            for(int c=0; c<b;c++)
+            for(int i=0; i < size(wiersz); i++)
             {
-                if(wyodrebnij(linia, i)==pomocnicza_tablica[c])
+                if(wiersz[i]==','){liczba_atrybutow++;}
+            }
+            string naglowek[liczba_atrybutow];//przechowuje nazwy nagłówków
+            for(int i=0; i < liczba_atrybutow; i++)
+            {
+                naglowek[i]=wyodrebnij(wiersz, i);
+            }   
+        }else{
+            dane.push_back({0,0,0,0,0,0,0,0,0,0});//dynamicznie powiekszam dane
+            for(int i=0; i<liczba_atrybutow; i++)
+            {
+                bin = wyodrebnij(wiersz, i);//bin przechowuje wartosc danej komór aby zmienszyć ilosć potrzebnych obliczeń
+                if(czy_duplikat(slownik, bin))//sprawdza czy zawartość komurki jest duplikatem
                 {
-                    cout << wyodrebnij(linia, i) <<endl;
-                    if(wyodrebnij(linia, 10)=="Pass"){dane[i][c][0]=dane[i][c][0]+1;}
-                    duplikat=1;
+                    dane[liczba_iteracji-1][i]=znajdz_duplikat(slownik, bin);//wpisuje id duplikatu
+                }
+                else
+                {
+                    dane[liczba_iteracji-1][i]=slownik.size();//wpisuje nowe id
+                    slownik.push_back(bin);//wpisuje string do slownik
                 }
             }
-            if(duplikat==0)
-            {
-                cout<<wyodrebnij(linia, i)<<endl;
-                pomocnicza_tablica[b]=wyodrebnij(linia, i);
-                if(wyodrebnij(linia, 10)=="Pass"){dane[i][b][0]=dane[i][b][0]+1;};
-                b++;
-            }
-            
         }
+        liczba_iteracji++;//śledzi dokonane iteracje
     }
     odczyt.close();
+    int liczba_wierszy=liczba_iteracji-1;//dla ułatwienia
+
+    
 
     return 0;
 }
